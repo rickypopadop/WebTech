e9a375b987b0b7b8bf565922d2c697526dd2f7ed
rickypopadop <124064058+rickypopadop@users.noreply.github.com>
2023-04-24 20:10:19 +0100

    Add files via upload

--- multipleFunctions.js
+++ multipleFunctions.js
@@ -11,6 +11,7 @@ function resetHidden(choice){
 	for (i=0; i<data["pancakes"].length; i++){
 		document.getElementById(data["pancakes"][i]["id"]).classList.remove("genericalHidden")
 	}
+
 }
 			
 
@@ -26,7 +27,67 @@ function buildPancake(){
 	fullMENU = ``
 	for (i=0; i<data["pancakes"].length; i++){
 		fullMENU+=onePancake(data["pancakes"][i])
+		console.log("he");
 	}
 
 	document.getElementById("continental").innerHTML = fullMENU
+}
+
+function homePage(){
+	window.location.href = "index.html"
+}
+function sanitizedID(id) {
+	if (!isNaN(id)){
+		if (id >= 1 && id <= data["pancakes"].length){
+			return id
+		}
+	}
+	window.location.href = "index.html"
+}
+
+function searchId(id){
+
+	for (i=0; i<data["pancakes"].length; i++){
+		if (data["pancakes"][i]["id"] == id){
+					
+			return data["pancakes"][i]
+		}
+	}
+	return null
+}
+function buildPage(p){
+	page=``
+	page+=`<div class="row">
+				<div class="column">
+					<div id="ingrediants">
+						<h2>Ingreediants</h2>
+						<p>`
+	for(i=0; i<p["ingredients"].length-1; i++){
+		page+=`${p["ingredients"][i]} <br>`
+	}
+	page+=`${p["ingredients"][p["ingredients"].length-1]} </p>`
+	page+=`</div>
+		</div>
+		<div class="column">
+		<img src="${p["img"]}" width=100%>
+	</div>
+	</div>
+	<div class="row">
+		<div class="column">
+			<div id="instructions">
+				<h2>Cooking instructions</h2>
+				<p>
+	`
+	for(i=0; i<p["instructions"].length-1; i++){
+		page+=`${p["instructions"][i]} <br>`
+	}
+	page+=`${p["instructions"][p["instructions"].length-1]} </p>
+		</div>
+		</div>
+	</div>
+
+`
+document.getElementById("pancakePage").innerHTML = page
+return null
+
 }
